95629dc407c5614da18d197abf9f7fac77461638
ajpung <83614749+ajpung@users.noreply.github.com>
2025-04-15 07:29:03 -0400

    chore: standing up initial CNN

--- src/generic/data/image_processing.py
+++ src/generic/data/image_processing.py
@@ -1,20 +1,18 @@
-from glob import glob
+from typing import Any
 
 import cv2
-from typing import Any
-import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.image import imread
 
 
-def make_bw_square(file: str) -> tuple[np.ndarray[Any, Any], np.ndarray[Any, Any]]:
+def make_bw_square(file: str) -> np.ndarray[tuple[int, ...], Any]:
     """
     Ingests an image file, reads dimensions, and forces image to be a black and white square image.
     :param file: Path to the image file
     :return new_image: Image as a numpy array
     """
     # Ingest images
-    image = imread(cur_img)
+    image = imread(file)
     px, py, pz = image.shape
     # Find minimum value between X and Y dimensions
     min_dim = min(px, py)
@@ -27,7 +25,7 @@ def make_bw_square(file: str) -> tuple[np.ndarray[Any, Any], np.ndarray[Any, Any
 
     # Force image to be square
     new_image = new_image[:min_dim, :min_dim]
-    return image, new_image
+    return new_image
 
 
 def resize_image(image: np.ndarray, new_dim: int) -> np.ndarray:
@@ -42,20 +40,3 @@ def resize_image(image: np.ndarray, new_dim: int) -> np.ndarray:
     # Resize the image using interpolation
     resized_img = cv2.resize(image, new_dimensions, interpolation=cv2.INTER_AREA)
     return resized_img
-
-
-# Find/ingest files in folder; force square & b/w
-files = glob("..\..\..\images\squared\*.png")
-for cur_img in files:
-    image_0, image_1 = make_bw_square(cur_img)
-    image_2 = resize_image(image_1, new_dim=288)
-
-# Demonstrate processing
-plt.figure()
-plt.subplot(1, 3, 1)
-plt.imshow(image_0)
-plt.subplot(1, 3, 2)
-plt.imshow(image_1, cmap="gray")
-plt.subplot(1, 3, 3)
-plt.imshow(image_2, cmap="gray")
-plt.show()
